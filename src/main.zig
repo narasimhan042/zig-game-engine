b10684a9a6e2579e818f70ad650176037bda2d75
narasimhan042 <wishnewideas@gmail.com>
2024-06-02 23:04:32 -0400

    Window creation successful

--- src/main.zig
+++ src/main.zig
@@ -12,4 +12,23 @@ pub fn main() !void {
         return error.SDLInitializationFailed;
     }
     defer c.SDL_Quit();
+
+    const window = c.SDL_CreateWindow("SDL2 Window", 100, 100, 1280, 800, 0);
+
+    if (window == null) {
+        c.SDL_Log("SDL_CreateWindow failed.", c.SDL_GetError());
+        return error.SDLWindowCreationFailed;
+    }
+    defer c.SDL_DestroyWindow(window);
+
+    var ev: c.SDL_Event = undefined;
+    var quit = false;
+
+    while (!quit) {
+        _ = c.SDL_PollEvent(&ev);
+
+        if (ev.type == c.SDL_QUIT) {
+            quit = true;
+        }
+    }
 }
