52ae9605e4cba11f229bf9f4b8c0880dddefbde2
narasimhan042 <wishnewideas@gmail.com>
2024-06-03 00:41:45 -0400

    adjusted project into engine lib

--- build.zig
+++ build.zig
@@ -15,100 +15,92 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    // const lib = b.addStaticLibrary(.{
-    //     .name = "sdl2-game",
-    //     // In this case the main source file is merely a path, however, in more
-    //     // complicated build scripts, this could be a generated file.
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
+    const lib = b.addStaticLibrary(.{
+        .name = "sdl2-game-engine",
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     // This declares intent for the library to be installed into the standard
     // location when the user invokes the "install" step (the default step when
     // running `zig build`).
-    // b.installArtifact(lib);
+    b.installArtifact(lib);
 
-    const exe = b.addExecutable(.{
-        .name = "sdl2-2d-game",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // if (target.query.isNativeOs() and target.query.os_tag == .linux) {
-    // The SDL package doesn't work for Linux yet, so we rely on system
-    // packages for now.
-    exe.linkSystemLibrary("SDL2");
-    // exe.addIncludePath("external/SDL2-2.30.3/include");
-    exe.linkLibC();
-
-    // } else {
-    //     const sdl_dep = b.dependency("sdl", .{
-    //         .optimize = .ReleaseFast,
-    //         .target = target,
-    //     });
-    //     exe.linkLibrary(sdl_dep.artifact("SDL2"));
-    // }
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-    // Wayland does not initialize even though libdecor is installed via dnf
-
-    // START OF ERROR
-    // libdecor-gtk-WARNING: Failed to initialize GTK
-    // Failed to load plugin 'libdecor-gtk.so': failed to init
-    // END OF ERROR
-    // Therefore the following fallback is neccessary
-    run_cmd.setEnvironmentVariable("SDL_VIDEODRIVER", "x11");
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_source_file = b.path("src/root.zig"),
+    // const exe = b.addExecutable(.{
+    //     .name = "sdl2-2d-game",
+    //     .root_source_file = b.path("src/main.zig"),
     //     .target = target,
     //     .optimize = optimize,
     // });
 
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    // // if (target.query.isNativeOs() and target.query.os_tag == .linux) {
+    // // The SDL package doesn't work for Linux yet, so we rely on system
+    // // packages for now.
+    // // exe.linkSystemLibrary("SDL2");
+    // // exe.linkLibC();
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+    // // // This declares intent for the executable to be installed into the
+    // // // standard location when the user invokes the "install" step (the default
+    // // // step when running `zig build`).
+    // // b.installArtifact(exe);
+
+    // // This *creates* a Run step in the build graph, to be executed when another
+    // // step is evaluated that depends on it. The next line below will establish
+    // // such a dependency.
+    // const run_cmd = b.addRunArtifact(exe);
+
+    // // Wayland does not initialize even though libdecor is installed via dnf
+
+    // // START OF ERROR
+    // // libdecor-gtk-WARNING: Failed to initialize GTK
+    // // Failed to load plugin 'libdecor-gtk.so': failed to init
+    // // END OF ERROR
+    // // Therefore the following fallback is neccessary
+    // run_cmd.setEnvironmentVariable("SDL_VIDEODRIVER", "x11");
+
+    // // By making the run step depend on the install step, it will be run from the
+    // // installation directory rather than directly from within the cache directory.
+    // // This is not necessary, however, if the application depends on other installed
+    // // files, this ensures they will be present and in the expected location.
+    // run_cmd.step.dependOn(b.getInstallStep());
+
+    // // This allows the user to pass arguments to the application in the build
+    // // command itself, like this: `zig build run -- arg1 arg2 etc`
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+
+    // // This creates a build step. It will be visible in the `zig build --help` menu,
+    // // and can be selected like this: `zig build run`
+    // // This will evaluate the `run` step rather than the default, which is "install".
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // const exe_unit_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/main.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
+    // test_step.dependOn(&run_exe_unit_tests.step);
 }
